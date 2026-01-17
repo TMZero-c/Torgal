0762449fc5b711ed30aa059d861aadfb85aac4ec
TMZero-c <alexmeimarcus@gmail.com>
2026-01-17 14:19:24 -0800

    init faster whisper

--- client/src/app/preload.js
+++ client/src/app/preload.js
@@ -8,3 +8,11 @@ window.addEventListener('DOMContentLoaded', () => {
     replaceText(`${type}-version`, process.versions[type])
   }
 })
+
+// preload.js
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  sendAudio: (buffer) => ipcRenderer.send('process-audio', buffer),
+  onTranscriptionResult: (callback) => ipcRenderer.on('transcription-data', callback)
+});
